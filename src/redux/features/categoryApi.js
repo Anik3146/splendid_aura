dfabb1b65bf9b550822e01fa8f1e06f84328c690
DESKTOP-O2KROQH\User <anikweb153@gmail.com>
2024-11-25 00:49:27 +0600

    baseurl changed

--- src/redux/features/categoryApi.js
+++ src/redux/features/categoryApi.js
@@ -5,17 +5,16 @@ export const categoryApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     addCategory: builder.mutation({
       query: (data) => ({
-        url: "https://splendid-aura-backend.vercel.app/api/category/add",
+        url: "http://localhost:7000/api/category/add",
         method: "POST",
         body: data,
       }),
     }),
     getShowCategory: builder.query({
-      query: () => `https://splendid-aura-backend.vercel.app/api/category/show`,
+      query: () => `http://localhost:7000/api/category/show`,
     }),
     getProductTypeCategory: builder.query({
-      query: (type) =>
-        `https://splendid-aura-backend.vercel.app/api/category/show/${type}`,
+      query: (type) => `http://localhost:7000/api/category/show/${type}`,
     }),
   }),
 });
